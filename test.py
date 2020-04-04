f23947148f8f38b1fc61bdc07457efc5ace5f499
OMALOE <56366333+OMALOE@users.noreply.github.com>
2020-04-04 20:54:26 +0300

    А

--- test.py
+++ test.py
@@ -7,15 +7,15 @@ with open("users.json", 'r', encoding='utf-8') as films:
 
 print(len(films_list))
 
-for user in films_list:
-    user["recommend"] = True
+# for user in films_list:
+#     user["recommend"] = True
 
-with open("users.json", 'w', encoding='utf-8') as films:
-    json.dump(films_list, films, ensure_ascii=False)
-# a = int(input())
-# if a == 0:
-#     sys.exit()
-# else:
-#     print("doesnt work")
+# with open("users.json", 'w', encoding='utf-8') as films:
+#     json.dump(films_list, films, ensure_ascii=False)
+# # a = int(input())
+# # if a == 0:
+# #     sys.exit()
+# # else:
+# #     print("doesnt work")
 
 # print("djvnjdnv")
